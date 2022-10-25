d7cc090f077a571e09c2e44b17f3655cefd9b304
MertCetin <mert.cetnn@gmail.com>
2022-10-24 20:48:52 -0400

    user model done ,bcrypted

--- models/comment.js
+++ models/comment.js
@@ -1,11 +1,11 @@
-// Here is where we set up our Post model, for when we are ready to connect to a database in future activities.
+// Here is where we set up our Comment model, for when we are ready to connect to a database in future activities.
 
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
-class Post extends Model {}
+class Comment extends Model {}
 
-Post.init(
+Comment.init(
   {
     id: {
       type: DataTypes.INTEGER,
@@ -13,25 +13,33 @@ Post.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    title: {
+    user_comment: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    content: {
-      type: DataTypes.STRING,
+    post_id: {
+      type: DataTypes.INTEGER,
       allowNull: false,
+      references: {
+        model: "user",
+        key: "id",
+      },
     },
     guest_id: {
-      type: DataTypes.STRING,
+      type: DataTypes.INTEGER,
       allowNull: false,
+      references: {
+        model: "post",
+        key: "id",
+      },
     },
   },
   {
     sequelize,
     freezeTableName: true,
     underscored: true,
-    modelName: "post",
+    modelName: "comment",
   }
 );
 
-module.exports = Post;
+module.exports = Comment;
