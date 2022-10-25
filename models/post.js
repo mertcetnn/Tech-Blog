03ff19ba6804acfd128d2c3b486368384922a752
MertCetin <mert.cetnn@gmail.com>
2022-10-24 20:31:23 -0400

    post.js model done

---
+++ models/post.js
@@ -0,0 +1,37 @@
+// Here is where we set up our Dish model, for when we are ready to connect to a database in future activities.
+
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Post extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    guest_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "dish",
+  }
+);
+
+module.exports = Dish;
